6e018dd8052f8fc69de59688f5542832864728ac
Ondra Machacek <omachace@redhat.com>
2017-11-30 18:56:11 +0100

    Add obsoletes to RPM spec

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION="1.1.2"
-MILESTONE=
-RPM_RELEASE="1"
+VERSION="1.1.3"
+MILESTONE=master
+RPM_RELEASE="0.1.$MILESTONE.$(date -u +%Y%m%d%H%M%S)"
 
 ROLE_NAME="ovirt.ansible-roles"
 PACKAGE_NAME="ovirt-ansible-roles"
